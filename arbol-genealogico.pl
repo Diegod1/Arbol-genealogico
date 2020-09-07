a0292d5bf889a47620277d6025e5dd8093c85ed4
Diego Alexander Delgado Maigual <diego@udenar.edu.co>
2020-09-07 15:24:37 -0500

    version 1.3

--- arbol-genealogico.pl
+++ arbol-genealogico.pl
@@ -1,9 +1,29 @@
-/*#declaracion de los hechos
-#1ra Generacion
-*/
-genero(florelia,mujer).
-genero(marina,mujer).
-genero(nelly,mujer).
+mujer(florelia).
+mujer(tatarabuela_david).
+mujer(tatarabuela_cordoba).
+mujer(tatarabuela_villota).
+mujer(tatarabuela_rosero).
+mujer(tatarabuela_pumalpa).
+mujer(tatarabuela_galves).
+mujer(tatarabuela_munoz).
+mujer(tatarabuela_rosero2).
+mujer(fidelina).
+mujer(maria).
+mujer(fidelina_pumalpa).
+mujer(carlina).
+mujer(marina).
+mujer(ester).
+mujer(nelly).
+mujer(aracelly).
+mujer(flor).
+mujer(stella).
+mujer(janeth).
+mujer(aida).
+mujer(johana).
+mujer(paola).
+mujer(diana).
+mujer(yuli).
+mujer(carol).
 
 %Diego es hijo de florelia
 hijo_de([diego],florelia).
@@ -12,6 +32,7 @@ hijo_de([carlos,fredy],flor).
 hijo_de([max,stiven],aimet).
 
 hijo_de([javier,andres],didimo).
+hijo_de([paola],aracelly).
 hijo_de([juan_jose],stella).
 hijo_de([juan_jose],luis_jr).
 hijo_de([diana],hernan).
@@ -70,15 +91,15 @@ hijo_de([alvaro],tatarabuela_rosero2).
 
 /*base de conocimientos (Reglas)*/
 %padre_de(Padre,Hijo):-hijo_de(Hijo,Padre).
-padre_de(Padre,Hijo):-hijo_de(Hijos,Padre),member(Hijo,Hijos).
-madre_de(Madre,Hijo):-hijo_de(Hijos,Madre),member(Hijo,Hijos),genero(Madre,mujer).
+padre_de(Padre,Hijo):-hijo_de(Hijos,Padre),member(Hijo,Hijos),not(mujer(Padre)).
+madre_de(Madre,Hijo):-hijo_de(Hijos,Madre),member(Hijo,Hijos),mujer(Madre).
+padres_de(Padre,Hijo):-hijo_de(Hijos,Padre),member(Hijo,Hijos).
 
-
-%hermanos
-hermano_de(H1,H2):-padre_de(X,H1);madre_de(X,H1);padre_de(X,H2);madre_de(X,H2),H1\==H2.
+%hermanos 
+hermano_de(H1,H2):-((padre_de(X,H1),padre_de(X,H2),H1\==H2);!),madre_de(X,H1),madre_de(X,H2),H1\==H2.
 
 %tios(Hermanos de los padres) X es padre de S Y X es hermano de T
-tio_de(T,S):-padre_de(X,S),hermano_de(X,T).
+tio_de(T,S):-padres_de(X,S),hermano_de(X,T).
 
 %sobrinos(hijos de mi hermano)
 
@@ -86,20 +107,9 @@ sobrino_de(S,T):-hermano_de(T,X),hijo_de(S,X).
 
 %primos(hijos de mis tios)
 
-primo_de(P1,P2):-padre_de(X,P1),tio_de(X,P2).
+primo_de(P1,P2):-padres_de(X,P1),tio_de(X,P2).
 %ancestros funcion recursiva
 
 %funcion para romper la recursividad
-ancestro_de(A,J):-(padre_de(A,J)).
-ancestro_de(A,J):-padre_de(A,Z),ancestro_de(Z,J).
-
-
-
-
-
-
-
-
-
-
-
+ancestro_de(A,J):-(padres_de(A,J)).
+ancestro_de(A,J):-padres_de(A,Z),ancestro_de(Z,J).
